31d24114cd439212cfddb63c1bbc354526f87dfa
luisxsssx <cd210902@gmail.com>
2023-11-02 20:47:04 -0700

    agregando el cors

--- src/main/java/com/example/demousers/DemoUsersApplication.java
+++ src/main/java/com/example/demousers/DemoUsersApplication.java
@@ -2,12 +2,28 @@ package com.example.demousers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DemoUsersApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoUsersApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:5173") // Reemplaza con la URL de tu frontend
+						.allowedMethods("GET", "POST", "PUT", "DELETE")
+						.allowedHeaders("*");
+			}
+		};
 	}
 
 }
